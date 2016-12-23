c5e7beb1a6a4ff4c9afa090e492d3c19481c11af
marcint87 <kulio2@wp.pl>
2016-12-23 15:06:26 +0100

    css changes for pixel perfect in navigation input

---
+++ js/main.js
@@ -0,0 +1,4 @@
+window.sr = ScrollReveal().reveal(' .text-section-heading, .text-section-text, .text-section-text-small, .about-heading, .about-text, .about-text-small', {duration: 1100, scale: 0.3});
+
+sr.reveal('.text-block', {distance: "50em"},50);
+// .text-block-wrapper,
