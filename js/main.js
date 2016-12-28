754f61da1c37389ce2efdcada1564e86ca8c5b1b
marcint87 <kulio2@wp.pl>
2016-12-28 11:04:35 +0100

    rwd changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 window.sr = ScrollReveal().reveal(' .text-section-heading, .text-section-text, .text-section-text-small, .about-heading, .about-text, .about-text-small', {duration: 1100, scale: 0.3});
 
 sr.reveal('.text-block', {distance: "50em"},50);
-// .text-block-wrapper,
+
+var menu =  document.getElementById("menu");
+var btn = document.getElementById("button");
+btn.addEventListener("click", function(e) {
+	if (menu.classList == "menu-list") {
+		 menu.className += " active";
+		 btn.className += " menu-active";
+	}
+	else {
+		menu.className = "menu-list";
+		btn.className = "menu-wrapper";
+	}
+});
